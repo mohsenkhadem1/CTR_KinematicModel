7910b71edc7620cd6d7dd0bde8df3992d00c2820
mohsenkhadem1 <39303711+mohsenkhadem1@users.noreply.github.com>
2020-05-28 10:19:42 +0100

    Update Example.py

--- Example.py
+++ Example.py
@@ -35,69 +35,10 @@ u_init = CTR.minimize(np.concatenate((u1_xy_0, uz_0), axis=None))
 C = CTR.comp(u_init)  # estimate compliance matrix
 J = CTR.jac(u_init)   # estimate jacobian matrix
 
-
-# Plotting the robot and principal axes of manipulability ellipsoids
-print("--- %s seconds ---" % (time.time() - start_time))
+# plot the robot shape
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-center = CTR.r[-1, :]
-# Stiffness matrix
-Lambda = np.diag(np.array([1, 1, 1]))
-VME = J[:, 0:4] @ J[:, 0:4].T  # Velocity manipulability
-CME = C @ C.T  # Compliance manipulability
-UME = C.T @ Lambda.T @ J @ J.T @ Lambda @ C  # Unified Force-Velocity manipulability
 
-# plotting compliance manipulability in green
-scale = 0.1 # scaling manipulability plot
-eig, eig_v = np.linalg.eig(CME)
-eig = np.sqrt(eig)
-X, Y, Z = zip(center, center, center)
-Vectors = np.array([scale * eig, scale * eig, scale * eig]).T @ eig_v
-p0 = np.array([[Vectors[0, 0], 0, 0], [Vectors[1, 0], 0, 0], [Vectors[2, 0], 0, 0]])
-U, V, W = zip(p0)
-print(C)
-ax.quiver(X, Y, Z, U, V, W, color='g',label='CME')
-p0 = np.array([[Vectors[0, 1], 0, 0], [Vectors[1, 1], 0, 0], [Vectors[2, 1], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='g')
-p0 = np.array([[Vectors[0, 2], 0, 0], [Vectors[1, 2], 0, 0], [Vectors[2, 2], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='g')
-
-# plotting force-velocity manipulability in red
-scale = 1  # scaling manipulability plot
-
-eig, eig_v = np.linalg.eig(UME)
-eig = np.sqrt(eig)
-X, Y, Z = zip(center, center, center)
-Vectors = np.array([scale * eig, scale * eig, scale * eig]).T @ eig_v
-p0 = np.array([[Vectors[0, 0], 0, 0], [Vectors[1, 0], 0, 0], [Vectors[2, 0], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='r',label='UME')
-p0 = np.array([[Vectors[0, 1], 0, 0], [Vectors[1, 1], 0, 0], [Vectors[2, 1], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='r')
-p0 = np.array([[Vectors[0, 2], 0, 0], [Vectors[1, 2], 0, 0], [Vectors[2, 2], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='r')
-
-# plotting velocity manipulability in yellow
-scale = 0.01  # scaling manipulability plot
-eig, eig_v = np.linalg.eig(VME)
-eig = np.sqrt(eig)
-X, Y, Z = zip(center, center, center)
-Vectors = np.array([scale * eig, scale * eig, scale * eig]).T @ eig_v
-p0 = np.array([[Vectors[0, 0], 0, 0], [Vectors[1, 0], 0, 0], [Vectors[2, 0], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='y',label='VME')
-p0 = np.array([[Vectors[0, 1], 0, 0], [Vectors[1, 1], 0, 0], [Vectors[2, 1], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='y')
-p0 = np.array([[Vectors[0, 2], 0, 0], [Vectors[1, 2], 0, 0], [Vectors[2, 2], 0, 0]])
-U, V, W = zip(p0)
-ax.quiver(X, Y, Z, U, V, W, color='y')
-
-# plot the robot shape
 ax.plot(CTR.r[:, 0], CTR.r[:, 1], CTR.r[:, 2], '-b',label='CTR Robot')
 ax.auto_scale_xyz([np.amin(CTR.r[:, 0]), np.amax(CTR.r[:, 0]) + 0.01],
                   [np.amin(CTR.r[:, 1]), np.amax(CTR.r[:, 1]) + 0.01],
